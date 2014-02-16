d7f00ef36a7039b1b8932b39fce833dad191f98e
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-02-15 21:29:07 -0500

    fixed globs so they don't build non slide presentations

--- slides/Gruntfile.js
+++ slides/Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
         },
         files: [{
           expand: true,
-          src: [ '**/*.jade' ],
+          src: [ '**/*.jade', '!**/starting*', '!shared/**' ],
           ext: '.html'
         }]
       }
